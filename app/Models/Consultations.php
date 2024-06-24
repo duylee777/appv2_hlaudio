b86c136e8e3e1585dc2ddc1d40798d47c2096d2a
kizorry <duyleit98@gmail.com>
2024-06-24 14:57:01 +0700

    update design web, policy

---
+++ app/Models/Consultations.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Consultations extends Model
+{
+    use HasFactory;
+
+    public $table = 'consultations';
+    
+    protected $fillable = [
+        'email', 'status', 'created_at', 'updated_at'
+    ];
+}
---
+++ resources/views/theme/policy/giao-hang.blade.php
@@ -0,0 +1,53 @@
+@extends('theme.layouts.page')
+@section('title','Chính sách giao hàng')
+@section('category-url', '')
+@section('category-name', '')
+@section('page-name', 'Chính sách giao hàng')
+@section('page-content')
+
+<div class="about-area mt-45 mb-35">
+    <div class="container">
+        <div class="row">
+            <div class="offset-lg-2 col-lg-8 text-center">
+                <div class="about-head">
+                    <h3 class="mb-20">Chính sách giao hàng</h3>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <p style="text-align:justify"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:12.0pt">Để đảm bảo việc cung cấp h&agrave;ng h&oacute;a một c&aacute;ch kịp thời, c&ugrave;ng với việc đảm bảo an to&agrave;n cho h&agrave;ng h&oacute;a khi vận chuyển, tạo sự tin tưởng cho qu&yacute; kh&aacute;ch h&agrave;ng, ch&uacute;ng t&ocirc;i xin th&ocirc;ng b&aacute;o ch&iacute;nh s&aacute;ch vận chuyển như sau:</span></span></span></p>
+
+            <ol>
+                <li style="text-align:justify"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:12.0pt">Tại H&agrave; Nội v&agrave; Th&agrave;nh phố Hồ Ch&iacute; Minh</span></span></span></li>
+            </ol>
+
+            <ul>
+                <li style="text-align:justify"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:12.0pt">Giao h&agrave;ng miễn ph&iacute; đối với c&aacute;c đơn h&agrave;ng kế hoạch trong b&aacute;n k&iacute;nh 20km</span></span></span></li>
+                <li style="text-align:justify"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:12.0pt">Hỗ trợ giao h&agrave;ng miễn ph&iacute; với c&aacute;c đơn h&agrave;ng kh&aacute;c c&oacute; thỏa thuận trước</span></span></span></li>
+                <li style="text-align:justify"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:12.0pt">C&aacute;c đơn h&agrave;ng nhỏ lẻ, đơn h&agrave;ng đột xuất qu&yacute; kh&aacute;ch vui l&ograve;ng thanh to&aacute;n chi ph&iacute; vận chuyển</span></span></span></li>
+            </ul>
+
+            <ol start="2">
+                <li style="text-align:justify"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:12.0pt">Tại c&aacute;c Tỉnh th&agrave;nh tr&ecirc;n cả nước</span></span></span></li>
+            </ol>
+
+            <ul>
+                <li style="text-align:justify"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:12.0pt">Đối với c&aacute;c đại l&yacute; k&iacute; hợp đồng, việc giao h&agrave;ng được quy định trong hợp đồng thương mại đ&atilde; được k&iacute; kết</span></span></span></li>
+                <li style="text-align:justify"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:12.0pt">Đối với c&aacute;c đơn h&agrave;ng ph&aacute;t sinh nhỏ, h&igrave;nh thức vận chuyển sẽ được thực hiện qua c&aacute;c đơn vị vận chuyển trung gian, qu&yacute; kh&aacute;ch vui l&ograve;ng thanh to&aacute;n chi ph&iacute; vận chuyển n&agrave;y.</span></span></span></li>
+            </ul>
+
+            <ol start="3">
+                <li style="text-align:justify"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:12.0pt">Tại c&aacute;c quốc gia kh&aacute;c</span></span></span></li>
+            </ol>
+
+            <ul>
+                <li style="text-align:justify"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:12.0pt">Đối với việc vận chuyển h&agrave;ng h&oacute;a sang quốc gia kh&aacute;c sẽ do đại l&yacute; tại c&aacute;c quốc gia chịu chi ph&iacute; v&agrave; phương tiện vận chuyển.</span></span></span></li>
+            </ul>
+
+            <p style="text-align:justify"><span style="font-size:11pt"><span style="font-family:Calibri,sans-serif"><span style="font-size:12.0pt">Lưu &yacute;: Tất cả c&aacute;c trường hợp trong qu&aacute; tr&igrave;nh vận chuyển diễn ra dẫn tới ảnh hưởng về chất lượng, ngoại h&igrave;nh sản phẩm qu&yacute; kh&aacute;ch h&agrave;ng vui l&ograve;ng li&ecirc;n hệ cho c&ocirc;ng ty để được giải quyết, muộn nhất 01 ng&agrave;y sau khi nhận được h&agrave;ng.</span></span></span></p>
+
+        </div>
+    </div>
+</div>
+
+@endsection
